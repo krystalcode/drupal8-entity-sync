67e88a413d9863100c1fdb0e29cf38143931b1cb
krystalcode <krystalcode@2392706.no-reply.drupal.org>
2020-05-06 01:51:36 +0000

    Issue #3130179 by krystalcode, shabana.navas: Client factory for generating sync clients

---
+++ src/Client/ClientInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\entity_sync\Client;
+
+/**
+ * Defines the interface for remote resource clients.
+ *
+ * Remote resource clients will be used to perform operations on the remote
+ * resource such as get the list of entities to import or update the remote
+ * entity that we are exporting.
+ *
+ * Currently, clients need to be made available as services and identified in
+ * the Sync's configuration so that the Client Factory will know how to generate
+ * the client.
+ *
+ * @see \Drupal\entity_sync\Client\ClientFactory
+ */
+interface ClientInterface {
+
+  /**
+   * Get the list of remote entities to import.
+   *
+   * @param array $options
+   *   An associative array of options. Supported options are:
+   *   - fromTime: (Optional) A Unix timestamp that when set, should limit the
+   *     remote entities to those created or updated after or at the given
+   *     timestamp.
+   *   - toTime: (Optional) A Unix timestamp that when set, should limit the
+   *     remote entities to those created or updated before or at the given
+   *     timestamp.
+   *
+   * @return \Iterator
+   *   An iterator containing the entities to import.
+   *
+   * @I Support paginated results
+   *    type     : improvement
+   *    priority : high
+   *    labels   : memory-consumption
+   */
+  public function importList(array $options = []);
+
+}
