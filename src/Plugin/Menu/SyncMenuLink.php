7f2696d01bb8bfb9d0972eee83eb450da306a0e8
Safallia Joseph <sjoseph@acromedia.com>
2020-05-07 11:28:00 -0700

    #3134170 - Add dynamic menu links for dynamic routes

---
+++ src/Plugin/Menu/SyncMenuLink.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\entity_sync\Plugin\Menu;
+
+use Drupal\Core\Menu\MenuLinkDefault;
+
+/**
+ * Represents a menu link for a single Sync operation.
+ */
+class SyncMenuLink extends MenuLinkDefault {}
