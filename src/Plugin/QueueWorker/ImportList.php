27e3773830eaa9e4e37e46693cffe88d5976eadf
Shabana Navas <snavas@acromedia.com>
2020-05-08 17:41:52 +0530

    Add functionality to queue worker to import teh entities.

--- src/Plugin/QueueWorker/ImportList.php
+++ src/Plugin/QueueWorker/ImportList.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\entity_sync\Plugin\QueueWorker;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Queue\QueueWorkerBase;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Queue worker for importing a list of entities.
@@ -18,18 +21,73 @@ class ImportList extends QueueWorkerBase implements
   ContainerFactoryPluginInterface {
 
   /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a new ImportList instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    ConfigFactoryInterface $config_factory
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition
+  ) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    *
-   * @I Add proper logic for performing the import
-   *    type     : bug
-   *    priority : normal
-   *    labels   : import, list
    * @I Add an option to be notified when the import is run
    *    type     : feature
    *    priority : normal
    *    labels   : import, list
    */
   public function processItem($data) {
+    $sync_id = $data['sync_id'];
+    $sync = $this->configFactory->get('entity_sync.sync.' . $sync_id);
+
+    // If the sync has defined a callback to use, use that.
+    if ($sync->get('manager_callback')) {
+      call_user_func('manager_callback');
+    }
+    // Otherwise, we call the default importRemoteList() service.
+    else {
+      \Drupal::service('entity_sync.import.manager')
+        ->importRemoteList($sync_id);
+    }
   }
 
 }
